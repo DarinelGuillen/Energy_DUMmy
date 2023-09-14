d79b640a4ae9a3412438e70dacb98bdadd3697c0
DarinelGuillen <darinel.escobar@outlook.com>
2023-09-14 15:01:58 -0600

    [ADD] LogIn view not finish yet

---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Test from "../components/organism/test";
+import "../assets/styles/LogIn.css"
+function LogIn() {
+    return ( 
+        <>
+        <h1>Hello World!</h1>
+        <Test/>
+        </>
+     );
+}
+
+export default LogIn;
